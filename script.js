f95803c7ddf858a1528f4b148822749c9e496fb0
jeh  maya <161092796+jehmaya@users.noreply.github.com>
2024-03-16 15:07:58 -0300

    projero descriptografar
    onde vocé  colocar sua frase e ele deixa uma frase  criptografada ,caso vocé queira ver sua sua mensagem e  so descriptografae ela

---
+++ script.js
@@ -0,0 +1,68 @@
+const textarea = document.querySelector(".area_texto");
+const mensagem = document.querySelector(".mensagem");
+
+
+// As "chaves" de criptografia que utilizaremos são:
+// A letra "e" é convertida para "enter"
+// A letra "i" é convertida para "imes"
+// A letra "a" é convertida para "ai"
+// A letra "o" é convertida para "ober"
+// A letra "u" é convertida para "ufat"
+
+function btnEncriptar() {
+    const textoEncriptado = encriptar(textarea.value);
+    mensagem.value = textoEncriptado;
+    textarea.value = "";
+}
+
+
+function encriptar(stringEncriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringEncriptada.includes(matrizCodigo[i][0])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
+        }
+    }
+
+    return stringEncriptada;
+}
+
+
+function btnDesencriptar() {
+    const textoDesencriptado = desencriptar(textarea.value);
+    mensagem.value = textoDesencriptado;
+    textarea.value = "";
+}
+
+
+function desencriptar(stringDesencriptada) {
+
+    let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
+    stringDesencriptada = stringDesencriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if(stringDesencriptada.includes(matrizCodigo[i][1])) {
+            stringDesencriptada = stringDesencriptada.replaceAll(matrizCodigo[i][1], matrizCodigo[i][0]);
+        }
+    }
+
+    return stringDesencriptada;
+}
+
+function copiarTexto() {
+    let copyText = document.getElementById('text-content');
+    copyText.select();
+    document.execCommand('copy');
+    openDialog("Texto copiado")
+}
+  
+  
+
+
+
+    
+  
+  
